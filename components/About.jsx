724e55f14614cf53fe90067b723614617a5d8f52
johncorrigan1 <jcorrigan322@gmail.com>
2022-10-23 18:01:34 -0400

    cart animation

--- components/About.jsx
+++ components/About.jsx
@@ -4,21 +4,20 @@ import Image from "next/image";
 const About = () => {
   return (
     <div className="about flex mt-10 justify-between">
-    <div className="flex flex-col text-6xl font-bold p-10">
-      <div className="flex gap-5 ">
-        <TextAnimation text="This" />
-        <TextAnimation text="Store" />
-        <TextAnimation text="is" />
+      <div className="flex flex-col text-6xl font-bold p-10">
+        <div className="flex gap-5 ">
+          <TextAnimation text="This" />
+          <TextAnimation text="Store" />
+          <TextAnimation text="is" />
+        </div>
+        <div className="flex gap-5">
+          <TextAnimation text="Completely" />
+          <TextAnimation text="Fake" />
+        </div>
       </div>
-      <div className="flex gap-5">
-        <TextAnimation text="Completely" />
-        <TextAnimation text="Fake" />
+      <div className="mr-10 shadow-lg shadow-slate-800 p-3 rounded-xl bg-slate-600 justify-center">
+        <Image className="" src="/fashionIcon.png" width={500} height={500} />
       </div>
-    </div>
-    <div className="mr-10 shadow-lg shadow-slate-800 p-3 rounded-xl bg-slate-600 justify-center">
-      <Image className="" src="/fashionIcon.png" width={500} height={500}/>
-      
-    </div>
     </div>
   );
 };
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,29 +1,54 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Image from "next/image";
-import CartModal from "./CartModal"
-import Link from 'next/link';
+import CartModal from "./CartModal";
+import Link from "next/link";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const [active, setActive] = useState(false)
 
-    const [open, setOpen] = useState(false)
+  const openCart = () => {
+    setOpen(true);
+    setActive(true);
+  };
 
-    const openCart = () => {
-        setOpen(true)
-    }
+   const closeHanlder = () => {
+        setActive(false);
+        setTimeout(() => {
+            setOpen(false)
+        }, 650)
+   }
 
-    return(
-        <div className="bg-slate-600 text-violet-100 shadow-md shadow-black">
-            <CartModal onClose={() => setOpen(false)} open={open} link="/shoe1.png" />
-            <nav className="flex justify-between mr-10 ml-10 p-5 items-center ">
-                <Link href="/"><h1 className="font-bold text-2xl hover:-translate-y-1 duration-300 cursor-pointer">Fake Store</h1></Link>
-                <ul className="flex gap-10">
-                    <Link href="/shop"><li className="text-lg font-semibold hover:-translate-y-1 cursor-pointer hover:underline duration-300">Shop</li></Link>
-                    <li onClick={openCart} className="text-lg font-semibold hover:-translate-y-1 cursor-pointer hover:underline duration-300">Cart</li>
-                    <Link href="/Contact"><li className="text-lg font-semibold hover:-translate-y-1 cursor-pointer hover:underline duration-300">Contact</li></Link>
-                </ul>
-            </nav>
-        </div>
-    )
-}
+  return (
+    <div className="bg-slate-600 text-violet-100 shadow-md shadow-black">
+      <CartModal onClose={closeHanlder} active={active} open={open} link="/shoe1.png" />
+      <nav className="flex justify-between mr-10 ml-10 p-5 items-center ">
+        <Link href="/">
+          <h1 className="font-bold text-2xl hover:-translate-y-1 duration-300 cursor-pointer">
+            Fake Store
+          </h1>
+        </Link>
+        <ul className="flex gap-10">
+          <Link href="/shop">
+            <li className="text-lg font-semibold hover:-translate-y-1 cursor-pointer hover:underline duration-300">
+              Shop
+            </li>
+          </Link>
+          <li
+            onClick={openCart}
+            className="text-lg font-semibold hover:-translate-y-1 cursor-pointer hover:underline duration-300"
+          >
+            Cart
+          </li>
+          <Link href="/Contact">
+            <li className="text-lg font-semibold hover:-translate-y-1 cursor-pointer hover:underline duration-300">
+              Contact
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
